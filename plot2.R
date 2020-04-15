e187e0ca41af101739bc4e88dd2306b44fd547b1
PhiPrime <phiprime1618@gmail.com>
2020-04-15 07:01:15 -0400

    Second Plot

--- plot2.R
+++ plot2.R
@@ -1,1 +1,18 @@
 #Line plot of Thu-Sat vs. Global Active Power
+generatePlot2 <- function() {
+        data <- read.csv("./data/condencedData.csv")
+        data <- mutate(data, Date_Time = as.POSIXct(strptime(
+                paste(data$Date, data$Time),
+                format = "%Y-%m-%d %H:%M:%S")))
+        png(file = "./plot2.png",
+             width = 480,
+             height = 480)
+        with(data, plot(Date_Time, Global_active_power,
+                        type = "l",
+                        xlab = " ",
+                        ylab = "Global Active Power (kilowatts)",
+                        ))
+        dev.off()
+}
+
+generatePlot2()
