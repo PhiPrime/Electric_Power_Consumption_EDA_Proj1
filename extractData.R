5d787f1b84d195df5495db3f81d9bd039478a55e
PhiPrime <phiprime1618@gmail.com>
2020-04-16 00:59:23 -0400

    added code to extract data

---
+++ extractData.R
@@ -0,0 +1,12 @@
+#Get data from zip file and create condencedData.csv
+saveLoc <- "./data/exdata_data_household_power_consumption.zip"
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+              saveLoc, method = "curl")
+unzip(saveLoc, exdir = "./data")
+allData <- read.csv("./data/household_power_consumption.txt",
+                    sep = ";", na.strings = "?")
+
+library(lubridate)
+condencedData <- allData[(dmy(allData$Date) == dmy("01/02/2007") |
+             dmy(allData$Date) == dmy("02/02/2007")),]
+write.csv(condencedData, "./data/condencedData.csv", row.names = FALSE)
