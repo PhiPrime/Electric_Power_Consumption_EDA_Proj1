b8bb0a20a9c25018ee3ddbebf6eff7edefc9562c
PhiPrime <phiprime1618@gmail.com>
2020-04-16 01:36:43 -0400

    Made into function

--- extractData.R
+++ extractData.R
@@ -1,4 +1,5 @@
 #Get data from zip file and create condencedData.csv
+extractData <- function() {
 saveLoc <- "./data/exdata_data_household_power_consumption.zip"
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
               saveLoc, method = "curl")
@@ -10,3 +11,4 @@ library(lubridate)
 condencedData <- allData[(dmy(allData$Date) == dmy("01/02/2007") |
              dmy(allData$Date) == dmy("02/02/2007")),]
 write.csv(condencedData, "./data/condencedData.csv", row.names = FALSE)
+}
